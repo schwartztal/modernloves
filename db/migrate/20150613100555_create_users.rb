fa577cc36ab08bb69f9708edd6be3a61a2125d14
Tal Schwartz <email": null>
2016-01-14 10:25:39 -0800

    complete db migrations, add env details and gitignore, begin styling the main page - navbar

--- db/migrate/20150613100555_create_users.rb
+++ db/migrate/20150613100555_create_users.rb
@@ -7,9 +7,16 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :full_name
+      t.string :first_name
+      t.string :last_name
       t.string :email, :null => false
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zip
       t.string :password_hash
       t.string :avatar_url
+      t.references :referrer, index: true
       t.timestamps null: false
     end
 
